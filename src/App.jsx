b66d745fed9056d4790a568c3bcd2d59e6a04ad5
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-06 20:26:43 -0300

    Ejercicio 1 Manejo de form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,73 @@
 import { useState } from "react";
 import "./App.css";
-import CoordenadaFlecha from "./components/CoordenadaFlecha";
 
 function App() {
-  const [visible, setVisible] = useState(true);
+  const [datos, setDatos] = useState({
+    nombre: "",
+    edad: "",
+    estudios: false,
+  });
 
-  function ocultar() {
-    setVisible(false);
+  function cambiarNombre(e) {
+    setDatos((valores) => ({
+      ...valores,
+      nombre: e.target.value,
+    }));
+  }
+  function cambiarEdad(e) {
+    setDatos((valores) => ({
+      ...valores,
+      edad: e.target.value,
+    }));
+  }
+  function cambiarEstudios(e) {
+    setDatos((valores) => ({
+      ...valores,
+      estudios: !datos.estudios,
+    }));
+  }
+
+  function procesar(e) {
+    e.preventDefault();
+    alert(
+      "Datos cargados" +
+        " " +
+        datos.nombre +
+        " " +
+        datos.edad +
+        " " +
+        datos.estudios
+    );
   }
 
   return (
     <div>
-      {visible ? <CoordenadaFlecha /> : <p>Se oculto la coordenada</p>}
-      <button onClick={ocultar}>Ocultar</button>
+      <form onSubmit={procesar}>
+        <p>
+          Ingrese su nombre:{" "}
+          <input type="text" value={datos.nombre} onChange={cambiarNombre} />
+        </p>
+        <p>
+          Ingrese su edad:{" "}
+          <input type="text" value={datos.edad} onChange={cambiarEdad} />
+        </p>
+        <p>
+          Estudios:{" "}
+          <input
+            type="checkbox"
+            value={datos.estudios}
+            onChange={cambiarEstudios}
+          />
+        </p>
+        <p>
+          <input type="submit" />
+        </p>
+      </form>
+      <hr />
+      <h3>Datos ingresados</h3>
+      <p>Nombre: {datos.nombre}</p>
+      <p>Edad: {datos.edad}</p>
+      <p>Estudios: {datos.estudios ? "Con estudios" : "Sin estudios"}</p>
     </div>
   );
 }
