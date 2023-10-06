d9354ddf8f848f0448ba5dc7d1160b4a8fcfe033
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-06 17:41:18 -0300

    Ejercicio 1 useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import ListadoResultados from "./components/ListadoResultados";
-import FormularioNumeros from "./components/FormularioNumeros";
 
 function App() {
-  const [operaciones, setOperaciones] = useState([]);
+  const [texto, setTexto] = useState("");
 
-  function sumar(event) {
-    event.preventDefault();
-    const v1 = parseInt(event.target.valor1.value);
-    const v2 = parseInt(event.target.valor1.value);
-    const suma = v1 + v2;
+  useEffect(() => {
+    document.title = texto;
+  }, [texto]);
 
-    const nuevo = {
-      resultado: suma,
-      valor1: v1,
-      valor2: v2,
-    };
-    setOperaciones([nuevo, ...operaciones]);
-
-    event.target.valor1.value = "";
-    event.target.valor2.value = "";
+  function cambiar(e) {
+    setTexto(e.target.value);
   }
 
   return (
     <div>
-      <FormularioNumeros onSumar={sumar} />
-      <ListadoResultados resultados={operaciones} />
+      <p>
+        <input type="text" onChange={cambiar} />
+      </p>
+      <p>{texto}</p>
     </div>
   );
 }
